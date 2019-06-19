66e5e578365dd01a1b26f89aac9e82b819b08bac
Diego Navarro <navarrow@gmail.com>
2019-06-19 16:01:32 -0300

    [navarrow] c06/metal: customization via --extra-vars & Fixes

--- c06/metal/mk-kubespray-cluster.sh
+++ c06/metal/mk-kubespray-cluster.sh
@@ -3,23 +3,30 @@
 #Software Needed [Ubuntu]
 sudo apt-get install -y git python3-venv
 git clone https://github.com/kubernetes-incubator/kubespray.git
-cd kubespray
 
 #Kubespray customization
 mkdir -p kubespray/inventory/cluster
 cp -r kubespray/inventory/sample/* kubespray/inventory/cluster
 pyvenv .venv && source .venv/bin/activate
 pip3 install --upgrade pip && pip3 install -r kubespray/requirements.txt && pip3 install ruamel.yaml
-sed -i "$ a\bootstrap_os: ubuntu" kubespray/inventory/cluster/group_vars/all/all.yml
-sed -i "$ a\kubeadm_enabled: true" kubespray/inventory/cluster/group_vars/all/all.yml
-sed -i "s/helm_enabled: false/helm_enabled: true/g" kubespray/inventory/cluster/group_vars/k8s-cluster/addons.yml
-sed -i "s/ingress_nginx_enabled: false/ingress_nginx_enabled: true/g" kubespray/inventory/cluster/group_vars/k8s-cluster/addons.yml
+
+cat > kubespray/inventory/cluster/local.yml << EOF
+bootstrap_os: ubuntu
+kubeadm_enabled: true
+helm_enabled: true
+ingress_nginx_enabled: true
 #Fix for https://github.com/kubernetes-sigs/kubespray/issues/4357
-sed -i "$ a\ingress_nginx_host_network: true" kubespray/inventory/cluster/group_vars/k8s-cluster/addons.yml
+ingress_nginx_host_network: true
+#Fix for https://github.com/kubernetes/kubernetes/pull/76640
+enable_nodelocaldns: false
+upstream_dns_servers:
+  - 8.8.8.8
+  - 8.8.4.4
+EOF
 
 cd kubespray
 
 #Declare IP for VMs
 declare -a IPS=(172.16.254.28 172.16.254.235 172.16.254.202 172.16.254.135)
 CONFIG_FILE=inventory/cluster/hosts.yml python3 contrib/inventory_builder/inventory.py ${IPS[@]}
-ansible-playbook -b -u linux -i inventory/cluster/hosts.yml cluster.yml
+ansible-playbook -b -u linux -i inventory/cluster/hosts.yml cluster.yml --extra-vars @inventory/cluster/local.yml
