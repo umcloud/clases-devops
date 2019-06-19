083c0e07b233776b998f10465704fd26071989a6
Diego Navarro <navarrow@gmail.com>
2019-06-19 01:57:00 -0300

    [navarrow] c06/{gke,metal} update

--- c06/gke/nginx-ingress-deploy-for-cloud-shell-cmds.sh
+++ c06/gke/nginx-ingress-deploy-for-cloud-shell-cmds.sh
@@ -1,5 +1,3 @@
-
-
 kubectl create clusterrolebinding cluster-admin-binding --clusterrole cluster-admin --user $(gcloud config get-value account)
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/mandatory.yaml
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/provider/cloud-generic.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/mandatory.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/cloud-generic.yaml
