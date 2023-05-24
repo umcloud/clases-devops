96745ec2e6c128044c4e7fdcf9ba4473d774ea48
JuanJo Ciarlante <juanjosec@gmail.com>
2023-05-23 21:11:42 -0300

    Added c01-terraform_intro example

---
+++ c01-terraform_intro/vm.tf
@@ -0,0 +1,23 @@
+data "openstack_images_image_v2" "ubuntu_2204" {
+  name        = "ubuntu_2204"
+  most_recent = true
+}
+
+data "openstack_compute_flavor_v2" "small" {
+  vcpus = 1
+  ram   = 2048
+}
+
+resource "openstack_compute_instance_v2" "terraform_vm" {
+  name              = "terraform_vm"
+  image_id          = data.openstack_images_image_v2.ubuntu_2204.id
+  flavor_id         = data.openstack_compute_flavor_v2.small.id
+  key_pair          = "REEMPLAZAR!" // <- reemplazar por el nombre de la keypair desde console.cloud.um.edu.ar
+  security_groups   = ["default"]
+  availability_zone = "nodos-amd-2022"
+
+  user_data = "${file("init.sh")}"
+  network {
+    name = "net_umstack"
+  }
+}
