b757091599326fcbdfc03318336402db73d540fc
JuanJo Ciarlante <juanjosec@gmail.com>
2024-05-29 20:04:35 -0300

    add c03-docker/p00 docker_vm via tofu, some other touches
    Signed-off-by: JuanJo Ciarlante <juanjosec@gmail.com>

---
+++ c03-docker/p00/init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+apt-get update -y
+apt-get install -y vim curl pv cmatrix jq
---
+++ c03-docker/p00/variables.tf
@@ -0,0 +1,4 @@
+variable "key_name" {
+  type    = string
+  default = "REEMPLAZAR!" // <- reemplazar por el nombre de la keypair desde console.cloud.um.edu.ar
+}
