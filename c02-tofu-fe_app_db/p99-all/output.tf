347666d8e389dba9e9abde451b68460e984e25d8
JuanJo Ciarlante <juanjosec@gmail.com>
2025-06-06 17:43:58 -0300

    moar output.tf
    Signed-off-by: JuanJo Ciarlante <juanjosec@gmail.com>

--- c02-tofu-fe_app_db/p99-all/output.tf
+++ c02-tofu-fe_app_db/p99-all/output.tf
@@ -6,7 +6,19 @@ output "tf_bastion_fip" {
   value = openstack_networking_floatingip_v2.tf_bastion_fip.address
 }
 
+output "tf_app_ip" {
+  value = openstack_compute_instance_v2.tf_app.network.0.fixed_ip_v4
+}
+
+output "tf_db_ip" {
+  value = openstack_compute_instance_v2.tf_db.network.0.fixed_ip_v4
+}
+
 output "tf_fe_ip" {
+  value = openstack_compute_instance_v2.tf_fe.network.0.fixed_ip_v4
+}
+
+output "tf_fe_fip" {
   value = openstack_networking_floatingip_v2.tf_fe_fip.address
 }
 
