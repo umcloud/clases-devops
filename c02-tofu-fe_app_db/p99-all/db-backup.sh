dca2c63a0ebd71d2b7fa44fde8465dc547627d16
JuanJo Ciarlante <juanjosec@gmail.com>
2025-06-06 22:46:36 -0300

    minor fixes
    Signed-off-by: JuanJo Ciarlante <juanjosec@gmail.com>

--- c02-tofu-fe_app_db/p99-all/db-backup.sh
+++ c02-tofu-fe_app_db/p99-all/db-backup.sh
@@ -2,4 +2,4 @@
 # Helper script to connect via bastion to the database server and dump all databases to stdout.
 CMD="env LC_ALL=C pg_dumpall -c"
 set -xeu
-ssh -CA ubuntu@${1:?missing bastion_ip} "ssh -C ${2:?missing db_ip} sudo -upostgres sh -xc \'${CMD}\'"
+ssh -CA "ubuntu@${1:?missing bastion_ip}" "ssh -C ${2:?missing db_ip} sudo -upostgres sh -xc \'${CMD}\'"
--- c02-tofu-fe_app_db/p99-all/db-restore.sh
+++ c02-tofu-fe_app_db/p99-all/db-restore.sh
@@ -2,4 +2,4 @@
 # Helper script to connect via bastion to the database server and restore a database from stdin.
 CMD="env LC_ALL=C psql -X -f-"
 set -xeu
-ssh -CA ubuntu@${1:?missing bastion_ip} "ssh -C ${2:?missing db_ip} sudo -upostgres sh -xc \'${CMD}\'"
+ssh -CA "ubuntu@${1:?missing bastion_ip}" "ssh -C ${2:?missing db_ip} sudo -upostgres sh -xc \'${CMD}\'"
