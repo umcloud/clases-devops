cb1bf710433b39e0f6885ba619cb3a4c8d147198
JuanJo Ciarlante <juanjosec@gmail.com>
2025-06-06 22:44:30 -0300

    add db-backup/restore output and scripts
    Signed-off-by: JuanJo Ciarlante <juanjosec@gmail.com>

---
+++ c02-tofu-fe_app_db/p99-all/db-restore.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Helper script to connect via bastion to the database server and restore a database from stdin.
+CMD="env LC_ALL=C psql -X -f-"
+set -xeu
+ssh -CA ubuntu@${1:?missing bastion_ip} "ssh -C ${2:?missing db_ip} sudo -upostgres sh -xc \'${CMD}\'"
--- c02-tofu-fe_app_db/p99-all/output.tf
+++ c02-tofu-fe_app_db/p99-all/output.tf
@@ -25,3 +25,11 @@ output "tf_fe_fip" {
 output "site_url" {
   value = "https://${replace(openstack_networking_floatingip_v2.tf_fe_fip.address, ".", "-")}.int.cloud.um.edu.ar/"
 }
+
+output "db_backup_cmd" {
+  value = "./db-backup.sh ${openstack_networking_floatingip_v2.tf_bastion_fip.address} ${openstack_compute_instance_v2.tf_db.network.0.fixed_ip_v4} > /tmp/db.dump"
+}
+
+output "db_restore_cmd" {
+  value = "./db-restore.sh ${openstack_networking_floatingip_v2.tf_bastion_fip.address} ${openstack_compute_instance_v2.tf_db.network.0.fixed_ip_v4} < /tmp/db.dump"
+}
